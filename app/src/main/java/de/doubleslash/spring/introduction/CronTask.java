312230acc66edcc37c22bb100b19bdd16261d3ea
sschalkowski <silvan.schalkowski@doubleslash.de>
2024-09-05 15:41:05 +0200

    Task #7
    Added:
    - Cronjob to delete  every five minutes every car-database-entry which is older than 24 hours

--- app/src/main/java/de/doubleslash/spring/introduction/CronTask.java
+++ app/src/main/java/de/doubleslash/spring/introduction/CronTask.java
@@ -1,15 +1,34 @@
 package de.doubleslash.spring.introduction;
 
+import de.doubleslash.spring.introduction.repository.CarRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 
 @AllArgsConstructor
 @Component
 public class CronTask {
 
-    //private final CarRepository repository;
+    @Autowired
+    private final CarRepository repository;
 
-
+    /**
+     * CronJob: Every 5 minutes
+     *                 ┌───────────── Seconds (0-59)
+     *                 │  ┌───────────── Minutes (0-59) (here: every 5 minutes)
+     *                 │  │  ┌─────────────── Hour (0-23)
+     *                 │  │  │ ┌───────────────── Day of month (1-31)
+     *                 │  │  │ │ ┌────────────────── Month (1-12 or Jan-Dec)
+     *                 │  │  │ │ │ ┌──────────────────── Day of the Week (0-7, both 0 and 7 represent Sunday)
+     */
+    @Scheduled(cron = "* */5 * * * *")
+    public void removeExpiredCars() {
+        repository.deleteAllByDateBefore(Instant.now().minus(24, ChronoUnit.HOURS));
+    }
 
 }
